f31760bf67a9739acc152f059e6e0bed4962c6bb
DESKTOP-3TKLI9Q\mocha <mochamad.rangga@gmail.com>
2022-11-23 21:01:35 +0700

    Setup Notification Setup Schedule

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -16,6 +16,13 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
+        $schedule->call(function(){
+            // Query atau Model
+
+            // Foreach
+
+            // Call Notification
+        })->daily();
     }
 
     /**
